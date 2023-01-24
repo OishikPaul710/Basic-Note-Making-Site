d58dabd7b79b2a518f3eb04e4af8796f8a1cd27d
Oishik Paul <88844855+OishikPaul710@users.noreply.github.com>
2023-01-24 16:38:20 +0530

    Add files via upload

---
+++ website/views.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    # Look for noteId that was passed to the function
+    # from the deleteNote function in index.js
+    # json.loads(request.data) - creates a Python dictionary object.
+    note = json.loads(request.data)
+    # Access the field noteID
+    noteId = note['noteId']
+    # Find the note from it's note ID
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
+
